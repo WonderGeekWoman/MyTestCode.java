9ea0769c8a351cd7ae81cfba7a4e9714bba70c6e
Amanda Halverson <amandahalverson@u.boisestate.edu>
2026-03-28 00:13:15 -0600

    License Updated

--- LicenseFromZyBooks/Database.java
+++ LicenseFromZyBooks/Database.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/** The program first reads integer licenseCount from input, representing the number of pairs of inputs to
+ * be read. Each pair has a string and an integer, representing the license's state and fee, respectively.
+ * One License object is created for each pair and added to ArrayList licenseList. Write the
+ * findAverageLicenseFee() method in the Database class to return the average fee of all the License
+ * objects as an integer. 
+ * 
+ * Ex: If the input is:
+ * 4
+ * OR 147 ND 120 TN 180 CA 245
+ * then the output is:
+ * Average license fee: 173
+ * 
+ * @author ZyBooks assignment
+ * */
+
 public class Database {
    private ArrayList<License> licenseList = new ArrayList<License>();
 
